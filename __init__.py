c0ab7001a8fd2df8a31d0e27a242a9ae6d28919d
ethonwilles <ethonwilles@gmail.com>
2020-06-24 17:10:28 -0600

    added some fixes

--- __init__.py
+++ __init__.py
@@ -3,24 +3,30 @@ from selenium.webdriver.chrome.options import Options
 from time import sleep
 
 from soup import scrape
+actual_path = "C:/Users/Owner/Desktop/chromedriver"
 class Driver():
     def __init__(self):
         self.options = Options()
         
         
-        self.driver = webdriver.Chrome(executable_path="C:/Users/Ethon/Desktop/chromedriver" , options=self.options)
+        self.driver = webdriver.Chrome(executable_path=actual_path , options=self.options)
     def auto(self):
         xpath = self.driver.find_element_by_xpath
         self.driver.get("https://www.msn.com")
+        sleep(1)
         outlook_btn = xpath('//*[@id="main"]/div[1]/div[1]/div/ul/li[1]/a')
         outlook_btn.click()
-        sleep(1)
+        sleep(2)
         new_window = self.driver.window_handles[1]
         self.driver.switch_to.window(new_window)
         try:
+            
             login_btn = xpath('//*[@id="office-Hero5050-e0h0pts"]/section/div/div[1]/div/div/div/div/div[2]/a')
             login_btn.click()
+            sleep(3)
+            print(self.driver.window_handles)
         except:
+            
             login_btn = xpath('/html/body/header/div/aside/div/nav/ul/li[2]/a')
             login_btn.click()
         sleep(2)
@@ -65,14 +71,19 @@ class Driver():
                         
                         alside_btn.click()
             except:
-                items = self.driver.find_elements_by_tag_name("div")
-                for item in items:
-                    if str(item.text) == "Larry Thatcher":
-                        item.click()
-                        scrape(self.driver.page_source)
-                        
-                        alside_btn.click()
-                        sleep(1)
+                try:
+                    items = self.driver.find_elements_by_tag_name("div")
+                    for item in items:
+                        if str(item.text) == "Larry Thatcher":
+                            item.click()
+                            scrape(self.driver.page_source)
+                            
+                            alside_btn.click()
+                            sleep(1)
+                except:
+                    print("all other tries have failed, attempting to restart...")
+                    alside_btn = xpath('//*[@id="app"]/div/div[2]/div[1]/div[1]/div[1]/div[2]/div/div[1]/div/div[2]/div[10]/div')
+                    alside_btn.click()
                     
 
             
